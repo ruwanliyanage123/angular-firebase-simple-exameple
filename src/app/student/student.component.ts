0f65ce20f9d8af40eafe24399e9e156cdc1507d2
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-04-06 09:01:22 +0530

    add functions for add  the data

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -12,6 +12,22 @@ export class StudentComponent implements OnInit {
   constructor(private service: StudentService, private db: AngularFirestore) { }
 
   ngOnInit() {
+    this.defaultData();
+  }
+
+  enterData(form: NgForm){
+    let data = form.value;
+    this.db.collection('student').add(data);
+  }
+
+  defaultData(form?: NgForm){
+    if(form!=null)
+    form.resetForm();
+    this.service.student ={
+      name: '',
+      age : '',
+      telephone: ''
+    }
   }
 
 }
